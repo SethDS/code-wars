5c7479218796a662c101b3c7956b81aca6a8b8ea
Seth Sutherland <cokolatepal@gmail.com>
2017-06-13 15:47:34 -0600

    perfect powers

--- solutions.js
+++ solutions.js
@@ -31,3 +31,12 @@ var isPP = function(n){
 
 
 };
+//Best solution NOT MINE
+function isPP(n) {
+    for (var m = 2; m * m <= n; ++ m)
+        for (var k = 2; Math.pow(m, k) <= n; ++ k)
+            if (Math.pow(m, k) == n) return [m, k];
+    return null;
+}
+
+isPP(25);
