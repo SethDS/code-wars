79216865bb069127e1af691db9456875349af5fb
Seth Sutherland <cokolatepal@gmail.com>
2017-07-11 22:34:31 -0600

    alhpabet wars

--- solutions.js
+++ solutions.js
@@ -40,3 +40,60 @@ function isPP(n) {
 }
 
 isPP(25);
+
+//Working on solution for level 5 alphabetWars problem
+function alphabetWar(battlefield) {
+    var str = battlefield.split(''); //console.log(str);//working
+    var groupArr = [];
+    var shelterCoordinates = [];
+    var bombCoordinates = [];
+    var lastLettersStading = [];
+
+    if(str.indexOf('#') === -1){
+        for(var i = 0; i < str.length; i++){
+            if(str[i] === '[' || str[i] === ']'){
+                str.splice(i,1);
+            }//end of if statement
+        }//end of for loop
+        return str.join('');
+    }//end of if statement
+    //everything here works like it should
+
+    str.forEach((x,y) => {
+        if(x === '[' || x === ']'){
+        shelterCoordinates.push(y);
+    } if(x === '#'){
+        bombCoordinates.push(y);
+    }
+}); //working
+    // console.log(bombCoordinates);
+
+    for(var j = 0; j < shelterCoordinates.length; j += 2) {
+
+        groupArr.push({
+            coors: [shelterCoordinates[j],shelterCoordinates[j + 1]],
+            dead: false,
+            bombZone: [],
+            letters: (function(){
+                var letters = [];
+                for(var l = shelterCoordinates[j] + 1; l < shelterCoordinates[j + 1]; l++){
+                    letters.push(str[l])
+                }
+                return letters.join('');
+            })()
+        });
+
+    };//working, groupArr looks the way it should
+
+    if(groupArr.length === 1 && bombCoordinates.length > 1){
+        return '';
+    } else if(groupArr.length === 2){
+
+    }
+
+//    for(var m = 0; m < groupArr.length; m++){
+
+//    }
+    console.log(groupArr)
+
+}
