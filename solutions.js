02045534aaf116eb7212b83fde52bdbd507b6ea0
Seth Sutherland <cokolatepal@gmail.com>
2017-07-12 14:24:48 -0600

    finished with alphabet wars nuclear strike

--- solutions.js
+++ solutions.js
@@ -41,14 +41,16 @@ function isPP(n) {
 
 isPP(25);
 
-//Working on solution for level 5 alphabetWars problem
+//Solution for level 5 Alphabet Wars Nuclear strike problem
 function alphabetWar(battlefield) {
     var str = battlefield.split(''); //console.log(str);//working
     var groupArr = [];
     var shelterCoordinates = [];
     var bombCoordinates = [];
     var lastLettersStading = [];
+    var returnString = [];
 
+    //if there are no bombs, splice out brackets and return all the letters
     if(str.indexOf('#') === -1){
         for(var i = 0; i < str.length; i++){
             if(str[i] === '[' || str[i] === ']'){
@@ -59,6 +61,7 @@ function alphabetWar(battlefield) {
     }//end of if statement
     //everything here works like it should
 
+    //get the coordinates for each of the shelters and bombs
     str.forEach((x,y) => {
         if(x === '[' || x === ']'){
         shelterCoordinates.push(y);
@@ -68,12 +71,13 @@ function alphabetWar(battlefield) {
 }); //working
     // console.log(bombCoordinates);
 
+    //loop through each pair of coordinates, create an object for each shelter with its coordinates,
+    //whether or not the letters are dead and each of the letters inside the shelter
     for(var j = 0; j < shelterCoordinates.length; j += 2) {
 
         groupArr.push({
             coors: [shelterCoordinates[j],shelterCoordinates[j + 1]],
             dead: false,
-            bombZone: [],
             letters: (function(){
                 var letters = [];
                 for(var l = shelterCoordinates[j] + 1; l < shelterCoordinates[j + 1]; l++){
@@ -83,17 +87,161 @@ function alphabetWar(battlefield) {
             })()
         });
 
-    };//working, groupArr looks the way it should
+    } //working, groupArr looks the way it should
 
-    if(groupArr.length === 1 && bombCoordinates.length > 1){
+    //checking to see if there is only one bomb, if so, return all letters in shelters
+    if(bombCoordinates.length === 1){
+        groupArr.map((x) => {
+            returnString.push(x.letters);
+    })
+        return returnString.join('');
+    } //working
+
+    //now, check to see if there is one shelter and more than one bomb, if true, return an empty string.
+    else if(groupArr.length === 1 && bombCoordinates.length > 1){
         return '';
-    } else if(groupArr.length === 2){
+    }  //working
 
-    }
+    //now, dealing with 2 shelters and more than one bomb
+    else if(groupArr.length === 2 && bombCoordinates.length > 1){
 
-//    for(var m = 0; m < groupArr.length; m++){
+        //check if more than one bomb in bombZone
+        var bombTickerOne = 0;
+        var bombTickerTwo = 0;
 
-//    }
-    console.log(groupArr)
+        for(var o = 0; o < bombCoordinates.length; o++){
+            //Increment the bomb ticker for the first shelter if the bomb is in its bombZone, but not in the second shelter's.
+            if(bombCoordinates[o] < groupArr[0].coors[0]){
+                bombTickerOne++;
+            }
+            //   Increment the bomb ticker for the second shelter if the bomb is in its bombZone, but not in the first's shelter.
+            if(bombCoordinates[o] > groupArr[1].coors[1]){
+                bombTickerTwo++;
+            }
+            //Increment the bomb ticker for both shelters if the bomb is in their shared bombZone;
+            if(bombCoordinates[o] > groupArr[0].coors[1] && bombCoordinates[o] < groupArr[1].coors[0]){
+                bombTickerOne++;
+                bombTickerTwo++;
+            }
+        }
 
-}
+        //check bombTicker amounts, if both are over one, return an empty string
+        if(bombTickerOne < 2 && bombTickerTwo < 2){
+            return  groupArr[0].letters + groupArr[1].letters;
+        }
+        else if(bombTickerOne > 1 && bombTickerTwo > 1){
+            return '';
+        } else if(bombTickerOne > 1 && bombTickerTwo < 2){  //if bombTickerOne is over one and bombTickerTwo isn't, return the letters from the second shelter joined in a string
+            return groupArr[1].letters;
+        } else if(bombTickerTwo > 1 && bombTickerOne < 2){ //if bombTickerTwo is over one and bombTickerOne isn't, return the letters from the first shelter joined in a string
+            return groupArr[0].letters;
+        }
+
+    } //this block of code has achieved its objective
+
+    //this is for when there are more than two shelters and more than one bomb
+    //this code will loop through groupArr and create a bombTicker for each shelter
+    //then, it will loop through groupArr again and check to see if any bomb coordinates fall within its bomb zone and increment its ticker in //bombTickers
+    else if(groupArr.length > 2 && bombCoordinates.length > 1){
+
+        var bombTickers = [];
+
+        for(var p = 0; p < groupArr.length; p++){ //creating a bombTicker for each shelter
+            bombTickers.push(0);
+        }
+
+        //loop through and check if any bombs fall inside the bombzone
+        for(var q = 0; q < groupArr.length; q++){
+
+            for(var r = 0; r < bombCoordinates.length; r++){
+
+                // //this is for the first item in the array
+                if(q === 0){
+                    if(bombCoordinates[r] < groupArr[q].coors[0]){
+                        bombTickers[q]++;
+                    } if(bombCoordinates[r] > groupArr[q].coors[1] && bombCoordinates[r] < groupArr[q + 1].coors[0]){
+                        bombTickers[q]++;
+                    }
+                }
+
+                //this is for the last item in the array
+                if(q === groupArr.length - 1){
+                    if(bombCoordinates[r] > groupArr[q].coors[1]){
+                        bombTickers[q]++;
+                    } if(bombCoordinates[r] < groupArr[q].coors[0] && bombCoordinates[r] > groupArr[groupArr.length - 2].coors[1]){
+                        bombTickers[q]++;
+                    }
+                }
+
+                //checking bomb positions for all items that are not first or last
+                if(q > 0 && q < groupArr.length - 1){
+                    if(bombCoordinates[r] < groupArr[q].coors[0] && bombCoordinates[r] > groupArr[q - 1].coors[1]){
+                        bombTickers[q]++;
+                    } if(bombCoordinates[r] > groupArr[q].coors[1] && bombCoordinates[r] < groupArr[q + 1].coors[0]){
+                        bombTickers[q]++;
+                    }
+                }
+
+            }//end of r for loop
+        }//end of q for loop
+
+        //now, checking which shelters have a bomb count under two, add their letters to a string and return that
+        for(var s = 0; s < groupArr.length; s++){
+            if(bombTickers[s] < 2){
+                returnString.push(groupArr[s].letters);
+            }
+        } //end of s for loop
+
+        return returnString.join('');
+
+    } //end of else if
+
+
+
+}  //end of function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
